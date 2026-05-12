cf632505009ec57fc27e4a75683c2761625a4781
ustakalakun <adamxh04@gmail.com>
2026-05-12 22:07:57 +0800

    make email notification for buyer to receive

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\BookRequest;
+use App\Mail\BookStatusMail;
+use Illuminate\Support\Facades\Mail;
+use App\Models\User;
 
 class RequestController extends Controller
 {
@@ -23,6 +26,13 @@ class RequestController extends Controller
         $request->book->update([
             'status' => 'sold'
         ]);
+
+        $requester = User::find($request->requester_id);
+
+        if ($requester) {
+            Mail::to($requester->email)
+                ->send(new BookStatusMail($request, 'completed'));
+        }
 
         return back()->with('success', 'Marked as taken!');
     }
